7faf0ca000f53dc8ab79d97f0731c0aa7311b1bb
Viktoria <zaharova181114@gmail.com>
2021-10-02 16:49:02 +0300

    add basket

--- app/js/common.js
+++ app/js/common.js
@@ -39,3 +39,19 @@ $('.form-selection input').on('keyup',function(){
         $('.input-text-size').css('opacity', '0');
     }
 });
+
+// amount
+$('.down').on("click", function () {
+    var $input = $(this).parent().find('input');
+    var count = parseInt($input.val()) - 1;
+    count = count < 1 ? 1 : count;
+    $input.val(count);
+    $input.change();
+    return false;
+});
+$('.up').on("click",function () {
+    var $input = $(this).parent().find('input');
+    $input.val(parseInt($input.val()) + 1);
+    $input.change();
+    return false;
+});
